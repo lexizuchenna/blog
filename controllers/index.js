ab0c8efef4e3472f67c11d24537ad2a6dbed4e4f
Alexander Ukwueze <lextechspec@gmail.com>
2022-10-17 20:37:26 +0100

    Links

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,14 @@
 const Posts = require("../models/Post");
 
 const viewHome = async (req, res) => {
-  // const posts = await Posts.find()
-  return res.status(200).render("home");
+  const posts = await Posts.find().lean()
+  const data = posts.map((post) => {
+    return post.category
+  })
+
+  const category = [... new Set(data)]
+
+  return res.status(200).render("home", {posts, category});
 };
 const viewAbout = async (req, res) => {
   return res.status(200).render("about");
